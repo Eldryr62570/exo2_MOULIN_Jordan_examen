08f952b267626ee2127e880f07f7b5cc379b1fcd
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-06-10 14:33:39 +0200

    Inclusion image et card selon bdd

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,14 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="app_main")
      */
-    public function index(): Response
+    public function index(ProjectRepository $projectRepository): Response
     {
-        return $this->render('main/index.html.twig');
+        $projects = $projectRepository->findAll();
+
+
+
+        return $this->render('main/index.html.twig',[
+            "projects" => $projects
+        ]);
     }
 }
--- src/Controller/Project/ProjectCrudController.php
+++ src/Controller/Project/ProjectCrudController.php
@@ -22,8 +22,8 @@ class ProjectCrudController extends AbstractCrudController
         return [
             TextField::new('name_project')->setLabel("Nom du projet"),
             TextareaField::new('description_projet')->setLabel("Description du projet"),
-            TextField::new('lien_github')->setLabel("Lien du github"),
-            TextField::new('lien_projet')->setLabel("Lien du projet"),
+            TextField::new('lien_github')->setLabel("Lien du github")->hideOnIndex(),
+            TextField::new('lien_projet')->setLabel("Lien du projet")->hideOnIndex(),
             ImageField::new('url_image')->setUploadedFileNamePattern('/build/uploads/[contenthash].[extension]')->setUploadDir('public/build/uploads')->setLabel('Image'),
             DateField::new('createdAt')->hideOnForm()->setLabel("Date de création"),
         ];

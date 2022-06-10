e03eebc211583abdd6e13af6844390b4745dfa49
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-06-10 16:27:09 +0200

    Formulaire sans ajax

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Document;
+use App\Form\DocumentType;
 use App\Repository\ProjectRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\DocumentRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
@@ -20,6 +25,22 @@ class MainController extends AbstractController
 
         return $this->render('main/index.html.twig',[
             "projects" => $projects
+        ]);
+    }
+    /**
+     * @Route("/document", name="document")
+     */
+    public function document(Request $request) : Response{ 
+        $document = new Document();
+        $form = $this->createForm(DocumentType::class, $document);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+
+
+        }
+        return $this->render('main/document.html.twig', [
+            'documentForm' => $form->createView(),
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -50,9 +50,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $projects;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Document::class, mappedBy="user")
+     */
+    private $documents;
+
     public function __construct()
     {
         $this->projects = new ArrayCollection();
+        $this->documents = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -180,6 +186,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
             // set the owning side to null (unless already changed)
             if ($project->getUser() === $this) {
                 $project->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Document>
+     */
+    public function getDocuments(): Collection
+    {
+        return $this->documents;
+    }
+
+    public function addDocument(Document $document): self
+    {
+        if (!$this->documents->contains($document)) {
+            $this->documents[] = $document;
+            $document->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDocument(Document $document): self
+    {
+        if ($this->documents->removeElement($document)) {
+            // set the owning side to null (unless already changed)
+            if ($document->getUser() === $this) {
+                $document->setUser(null);
             }
         }
 
